35d48be0b29c94a41da21410da8d2266d2fbbd97
yansuowhb <2081720759@qq.com>
2020-08-24 09:55:04 +0800

    创建文件

---
+++ 02-firstbili.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         同传助手
+// @namespace    http://tampermonkey.net/
+// @version      7.1.1
+// @description  同传助手
+// @author       烟锁池塘柳
+// @license      MIT License
+// @include      /https?:\/\/live\.bilibili\.com\/\d+\??.*/
+// @require      https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js
+// @require      https://cdn.bootcss.com/layer/2.4/layer.js
+// @require      https://greasyfork.org/scripts/406141-bilibiliapi-1-4-6/code/BilibiliAPI_146.js
+// @require      https://greasyfork.org/scripts/406185-ocrad2-3-2/code/OCRAD232.js
+// ==/UserScript==
+/*
+[github源]
+// @require      https://raw.githubusercontent.com/sakamaki-izayoi-LYN/API/master/BilibiliAPI.js
+// @require      https://raw.githubusercontent.com/sakamaki-izayoi-LYN/API/master/OCRAD.min.js
+[腾讯云源]
+// @require      https://js-1258131272.file.myqcloud.com/BilibiliAPI.js
+// @require      https://js-1258131272.file.myqcloud.com/OCRAD.min.js
+[jsDelivr源]
+// @require      https://cdn.jsdelivr.net/gh/sakamaki-izayoi-LYN/API/BilibiliAPI.js
+// @require      https://cdn.jsdelivr.net/gh/sakamaki-izayoi-LYN/API/OCRAD.min.js
+[GitCDN源]
+// @require      https://gitcdn.link/repo/sakamaki-izayoi-LYN/API/master/BilibiliAPI.js
+// @require      https://gitcdn.link/repo/sakamaki-izayoi-LYN/API/master/OCRAD.min.js
+*/
+
+
+var userInfo={
+    userId: '',
+    token:'',
+}
+
+
+$(function () {
+
+})
